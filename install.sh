4c223644ae1fb6e2ce530d8b8a841f83e397099b
Will Gordon <wpg4665@users.noreply.github.com>
2014-08-26 09:59:28 -0400

    Modified some packages choices

--- install.sh
+++ install.sh
@@ -10,14 +10,18 @@ echo 'deb http://debian.mirror.frontiernet.net/debian/ sid main contrib non-free
 # Update package lists
 aptitude -q update
 
-# Force python3 to install quietly
-DEBIAN_FRONTEND=noninteractive aptitude install -y -q python3.4-dev python-pip
+# Add python3 packages
+aptitude install -y -q python3.4-dev python-pip python-imaging
 
 # Replace python2 with python3 as symlinked python
 ln -sf /usr/bin/python3.4 /usr/bin/python
 
 # Install some newer system packages
-DEBIAN_FRONTEND=noninteractive aptitude install -y -q build-essential libtext-charwidth-perl libuuid-perl liblocale-gettext-perl libtext-iconv-perl libxml2-dev libssl-dev libjpeg8-dev libpng12-dev libfreetype6-dev libldap2-dev libmcrypt-dev libxslt-dev
+aptitude install -y -q build-essential libtext-charwidth-perl libuuid-perl 
+aptitude install -y -q liblocale-gettext-perl libtext-iconv-perl libxml2-dev 
+aptitude install -y -q libssl-dev libjpeg8-dev libpng12-dev libfreetype6-dev 
+aptitude install -y -q libldap2-dev libmcrypt-dev libxslt-dev
+aptitude install -y -q libjpeg8 libjpeg8-dev build-dep 
 
 # Remove sid
 sed -ie '$d' /etc/apt/sources.list
