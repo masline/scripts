f52e61773a770f5718ff8bb542275d122bb89793
Will Gordon <wpg4665@users.noreply.github.com>
2014-08-26 09:33:11 -0400

    Force symbolic link

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ aptitude -q update
 DEBIAN_FRONTEND=noninteractive aptitude install -y -q python3.4-dev
 
 # Replace python2 with python3 as symlinked python
-ln -s /usr/bin/python3.4 /usr/bin/python
+ln -sf /usr/bin/python3.4 /usr/bin/python
 
 # Install some newer system packages
 aptitude install -y -q build-essential libtext-charwidth-perl libuuid-perl liblocale-gettext-perl libtext-iconv-perl libxml2-dev libssl-dev libjpeg8-dev libpng12-dev libfreetype6-dev libldap2-dev libmcrypt-dev libxslt-dev
