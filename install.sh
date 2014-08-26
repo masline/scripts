40a86eb7cfa9d708efc66a9b767a357b4106b332
Will Gordon <wpg4665@users.noreply.github.com>
2014-08-26 09:34:45 -0400

    Added python-pip to install from sid

--- install.sh
+++ install.sh
@@ -11,13 +11,13 @@ echo 'deb http://debian.mirror.frontiernet.net/debian/ sid main contrib non-free
 aptitude -q update
 
 # Force python3 to install quietly
-DEBIAN_FRONTEND=noninteractive aptitude install -y -q python3.4-dev
+DEBIAN_FRONTEND=noninteractive aptitude install -y -q python3.4-dev python-pip
 
 # Replace python2 with python3 as symlinked python
 ln -sf /usr/bin/python3.4 /usr/bin/python
 
 # Install some newer system packages
-aptitude install -y -q build-essential libtext-charwidth-perl libuuid-perl liblocale-gettext-perl libtext-iconv-perl libxml2-dev libssl-dev libjpeg8-dev libpng12-dev libfreetype6-dev libldap2-dev libmcrypt-dev libxslt-dev
+DEBIAN_FRONTEND=noninteractive aptitude install -y -q build-essential libtext-charwidth-perl libuuid-perl liblocale-gettext-perl libtext-iconv-perl libxml2-dev libssl-dev libjpeg8-dev libpng12-dev libfreetype6-dev libldap2-dev libmcrypt-dev libxslt-dev
 
 # Remove sid
 sed -ie '$d' /etc/apt/sources.list
